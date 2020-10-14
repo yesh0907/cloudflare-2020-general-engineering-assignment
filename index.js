ceed1f7284ea7c909fb7b1f3882771d71bf2605f
Yesh Chandiramani <yesh0907@hotmail.com>
2020-10-14 16:01:17 +0800

    Add extra credit features

--- index.js
+++ index.js
@@ -13,11 +13,18 @@ const staticHTMLUrl = 'https://static-links-page.signalnerve.workers.dev'
 const profileImgSrc = 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Round&hairColor=BrownDark&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Brown'
 // My Username
 const username = 'yesh0907'
+// My Name
+const name = "Yesh Chandiramani"
+// New Bg color class from Tailwind CSS
+const bgColorClass = "bg-purple-400"
 
 // HTMLRewritter object to transform static html page
 const rewritter = new HTMLRewriter()
+  .on('title', new TitleTransformer()) // Extra Credit
+  .on('body', new BgColorTransformer()) // Extra Credit
   .on('div#links', new LinksTransformer(links))
-  .on('div#profile', new DisplayTransfomer())
+  .on('div#profile, div#social', new DisplayTransformer()) // div#social is Extra Credit
+  .on('div#social', new TwitterSocialTransformer())
   .on('img#avatar', new ProfileTransformer())
   .on('h1#name', new NameTransformer())
 
@@ -87,7 +94,7 @@ class LinksTransformer {
   }
 }
 
-class DisplayTransfomer {
+class DisplayTransformer {
   element(element) {
     // Remove the display: none in the style attribute of the element
     element.setAttribute('style', '')
@@ -105,6 +112,34 @@ class NameTransformer {
   element(element) {
     // Change the text of the element to the username
     element.setInnerContent(username)
+  }
+}
+
+// Extra Credit
+class TwitterSocialTransformer {
+  // Add Twitter SVG and link to Twitter
+  element(element) {
+    element.append(
+      `<a href="https://twitter.com">
+        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter icon</title><path d="M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z"></path></svg>
+      </a>`, { html: true }
+    )
+  }
+}
+
+// Extra Credit
+class TitleTransformer {
+  element(element) {
+    // Change the text of the title to the name
+    element.setInnerContent(name)
+  }
+}
+
+// Extra Credit
+class BgColorTransformer {
+  element(element) {
+    // Set the class to new bg color class
+    element.setAttribute('class', bgColorClass)
   }
 }
 
